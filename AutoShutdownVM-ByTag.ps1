18d6a845ecbd87b1973bf583d2b909a980fdec9f
Kelvin Kam <kelvinkam@live.hk>
2023-02-10 11:29:02 +0800

    New initial

---
+++ AutoShutdownVM-ByTag.ps1
@@ -0,0 +1,34 @@
+try
+{
+	# Ensures you do not inherit an AzContext in your runbook
+	$null = Disable-AzContextAutosave -Scope Process
+    "Logging in to Azure..."
+	# Connect to Azure with system-assigned managed identity
+	$AzureContext = (Connect-AzAccount -Identity).context
+	Write-Output $AzureContext
+    Write-Output $AzureContext.Subscription
+	# Set and store context
+	$AzureContext = Set-AzContext -Subscription $AzureContext.Subscription -DefaultProfile $AzureContext
+    #$AzureContext = Set-AzContext -Subscription "70e4e58a-b715-4463-8e6e-88c427e3e99e" -DefaultProfile $AzureContext
+    "Logged in."
+}
+catch {
+    if (!$AzureContext)
+    {
+        $ErrorMessage = "Connection $AzureContext not found."
+        throw $ErrorMessage
+    } else{
+        Write-Error -Message $_.Exception
+        throw $_.Exception
+    }
+}
+
+$ShutdownVMs = Get-AzResource -ResourceType Microsoft.Compute/virtualMachines | Select-Object Name,ResourceGroupName,@{Label="Tags-AutoShutdown";Expression={$_.Tags["AutoShutdown"]}} | Where-Object Tags-Autoshutdown -ne "False"
+$ShutdownVMs = $ShutdownVMs | Get-AzVM -status | Select-Object Name,ResourceGroupName,@{Label="Status";Expression={$_.Statuses[1].DisplayStatus}} | Where-Object {($_.Status -ne "VM deallocated") -or ($_.Status -ne "VM deallocating")}
+Write-Output "Shutdown virtual machines summary:`n "$ShutdownVMs.Name" `n"
+
+Foreach ($ShutdownVM in $ShutdownVMs){
+    Write-Output "Working on... $ShutdownVM"
+    $ShutdownVM | Stop-AzVM -Force -NoWait
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Azure-ShutdownVMByTag
 Shutdown Azure virtual machine by tags
+
+Usage
+Copy to Azure Automation Account
+
+Requirement
+Managed Identity
